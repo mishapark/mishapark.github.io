62624e4af03a1a22db377241f9126222c6e6d175
Mikhail Park <packmisha@mail.ru>
2020-06-26 13:20:34 +0300

    abc

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,9 @@
     // Get the modal
-    const modal = document.getElementsByClassName("hamburger__overlay");
+    const modal = document.querySelector("#hamburger__overlay");
     // Get the button that opens the modal
-    const btn = document.getElementsByClassName("menu__btn");
+    const btn = document.querySelector("#menu__btn");
     // Get the <span> element that closes the modal
-    const close = document.getElementsByClassName("hamburger__close")[0];
+    const close = document.querySelector("#hamburger__close")[0];
 
 
     
@@ -12,14 +12,9 @@
         modal.style.display = "flex";
     })
     
-    // When the user clicks on <span> (x), close the modal
-    close.onclick = function() {
-      modal.style.display = "none";
-    }
+  //   // When the user clicks on <span> (x), close the modal
+  //   close.addEventListener("click", e => {
+  //     modal.style.display = "none";
+  // })
     
     // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
