0e6a98ea5c62c64e4392ba173b8248e84f3bad88
Mikhail Park <packmisha@mail.ru>
2020-06-27 14:59:23 +0300

    slider

--- js/modal.js
+++ js/modal.js
@@ -3,7 +3,7 @@
     // Get the button that opens the modal
     const btn = document.querySelector("#menu__btn");
     // Get the <span> element that closes the modal
-    const close = document.querySelector("#menu__close")[0];
+    const close = document.querySelector("#menu__close");
 
 
     
@@ -13,9 +13,9 @@
     })
     
     // When the user clicks on <span> (x), close the modal
-    // close.addEventListener("click", e => {
-    //     modal.style.display = "none";
-    // })
+    close.addEventListener("click", e => {
+        modal.style.display = "none";
+    })
     
     modal.addEventListener("click", e => {
       if(e.target == modal) {
