4c9c742e00a9a6bba9170c002b96c8aa2c20ca2e
Mikhail Park <packmisha@mail.ru>
2020-07-01 19:33:36 +0300

    reviews done

--- js/reviews.js
+++ js/reviews.js
@@ -1,5 +1,19 @@
-const buttons = $(".reviews__list");
-buttons.on("click", function(e) {
-    e.preventDefault();
-  
-})
+let buttons = $(".reviews__item");
+
+let tabs = $(".reviews__content");
+
+for (let i = 0; i < buttons.length; i++) {
+    const element = buttons[i];
+    element.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        for (let index = 0; index < buttons.length; index++) {
+            buttons[index].classList.remove("reviews__item--active");
+            tabs[index].classList.remove("reviews__content--active");
+        }
+
+        tabs[i].classList.add("reviews__content--active");
+
+        e.currentTarget.classList.add("reviews__item--active");
+    })
+}
