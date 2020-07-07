4d59e7350f06329520cde497e13d84a6661953ee
Mikhail Park <packmisha@mail.ru>
2020-07-07 17:28:11 +0300

    colors done w/o cross

---
+++ js/colors.js
@@ -0,0 +1,27 @@
+function Accordeon(selector) {
+    const acco = document.querySelector(selector);
+    const items = acco.querySelector('[data-list]').children;
+
+    acco.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = e.target.closest('[data-trigger]');
+
+        if (!target) return;
+        
+        const item = target.parentNode;
+
+        if (item.classList.contains('active')) {
+            item.classList.remove('active');
+        } else {
+            for (let i = 0; i < items.length; i++) {
+                items[i].classList.remove('active');
+            };
+
+            item.classList.add('active');
+        }
+    });
+
+    
+}
+
+new Accordeon('#acc-menu');
