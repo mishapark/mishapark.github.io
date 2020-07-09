73d1a5f7576ad4a31e42c373042d7d81b2cf08bf
Mikhail Park <packmisha@mail.ru>
2020-07-09 18:29:15 +0300

    тцуц

--- js/player.js
+++ js/player.js
@@ -12,3 +12,34 @@ function togglePlayPause () {
 playButton.on("click", e => {
     togglePlayPause();
 })
+
+
+
+////////////////////////////////////
+
+
+
+// interval = setInterval(() => {
+//     const completedSec = player.getCurrentTime();
+//     const completedPercent = (completedSec / durationSec) * 100;
+  
+//     $(".playback__bar--past").css({
+//       width: `${completedPercent}%`
+//     });
+   
+
+//   $(".playback__bar").click(e => {
+//     const bar = $(e.currentTarget);
+//     const clickedPosition = e.originalEvent.layerX;
+    
+//     const newButtonPositionPercent = (clickedPosition / bar.width()) * 100;
+//     const newPlaybackPositionSec =
+//       (player.getDuration() / 100) * newButtonPositionPercent;
+    
+//     $(".playback__bar--past").css({
+//         width: `${newButtonPositionPercent}%` 
+//     });
+    
+//     player.seekTo(newPlaybackPositionSec);
+//    });
+// });
