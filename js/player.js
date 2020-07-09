673a8690b777d2632b22dc135ca46af03c95cb54
Mikhail Park <packmisha@mail.ru>
2020-07-09 18:04:44 +0300

    new

--- js/player.js
+++ js/player.js
@@ -1,3 +1,14 @@
 const video = $(".player__video");
 const playerContainer = $(".player");
- 
+const playButton = $("#play-pause");
+
+function togglePlayPause () {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+};
+playButton.on("click", e => {
+    togglePlayPause();
+})
