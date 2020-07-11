6afd298855f6a00355e5909b487b166f9648f2b3
Mikhail Park <packmisha@mail.ru>
2020-07-11 21:09:40 +0300

    fancybox problem

--- gulp.config.js
+++ gulp.config.js
@@ -4,9 +4,13 @@ module.exports = {
     STYLES_LIBS : [
         "node_modules/normalize.css/normalize.css",
         "node_modules/slick-carousel/slick/slick.css",
+        // "node_modules/fancybox/dist/css/jquery.fancybox.css"
     ],
     JS_LIBS : [
         "node_modules/jquery/dist/jquery.js",
-        "node_modules/slick-carousel/slick/slick.js"
+        "node_modules/slick-carousel/slick/slick.js",
+        "node_modules/mobile-detect/mobile-detect.js",
+        "node_modules/jquery-touchswipe/jquery.touchSwipe.js",
+        // "node_modules/fancybox/dist/js/jquery.fancybox.js"
     ]
 }
