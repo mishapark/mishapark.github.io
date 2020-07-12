e18158ade058a9466ea05a7844b7a207709ba2b6
Mikhail Park <packmisha@mail.ru>
2020-07-13 02:07:24 +0300

    paths

--- src/js/map.js
+++ src/js/map.js
@@ -18,7 +18,7 @@ const init = () => {
       const myCollection = new ymaps.GeoObjectCollection({}, {
         draggable: false,
         iconLayout: 'default#image',
-        iconImageHref: '/img/pics/map.svg',
+        iconImageHref: './img/pics/map.svg',
         iconImageSize: [46, 57],
         iconImageOffset: [-35, -52]
       });
